435a716a841925876ede4491d75b87f70b86e7e2
hntxuanit15spkt <hntxuanit15spkt@gmail.com>
2018-05-12 20:52:14 +0700

    gradle

--- app/src/main/java/vn/com/canhtoan/Database/Converter/DateConverter.java
+++ app/src/main/java/vn/com/canhtoan/Database/Converter/DateConverter.java
@@ -27,6 +27,16 @@ public class DateConverter {
     }
 
     @TypeConverter
+    public Date fromTimestamp(Long value) {
+        return value == null ? null : new Date(value);
+    }
+
+    @TypeConverter
+    public Long dateToTimestamp(Date date) {
+        return date == null ? null : date.getTime();
+    }
+
+    @TypeConverter
     public static Long toTimestamp(Date date) {
         return date == null ? null : date.getTime();
     }
